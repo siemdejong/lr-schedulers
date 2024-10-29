1dc28fba5395555ceed44acc42eefc210d049efe
siemdejong <28396796+siemdejong@users.noreply.github.com>
2024-10-29 19:50:40 +0100

    style: apply ruff

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+"""Streamlit application to visualize learning rate schedulers."""
+
+import ast
 import inspect
 import typing
-from typing import get_origin, get_args
-from typing import Callable as TCallable, _CallableGenericAlias
-from collections.abc import Iterable, Callable
+from collections.abc import Callable, Iterable
+from collections.abc import Callable as TCallable
+from typing import _CallableGenericAlias, get_args, get_origin
 
-import pandas as pd
-import streamlit as st
-import torch
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
+import streamlit as st
+import torch
 from torch.optim import SGD
-import numpy as np
 
 STEPS = 100
 
@@ -28,11 +30,14 @@ DEFAULTS = {
 
 Callable = Callable | TCallable | _CallableGenericAlias
 
-def get_func_as_string(func):
-    return inspect.getsourcelines(func)
 
 @st.cache_data
-def plot_schedule(scheduler_name, scheduler_cls: torch.optim.lr_scheduler.LRScheduler, parameters: dict):
+def plot_schedule(
+    scheduler_name: str,
+    scheduler_cls: torch.optim.lr_scheduler.LRScheduler,
+    parameters: dict,
+) -> go.Figure:
+    """Plot the learning rate schedule for a given scheduler."""
     optimizer = SGD([torch.tensor(1)], lr=1)
     # Use a scheduler of your choice below.
     # Great for debugging your own schedulers!
@@ -42,14 +47,14 @@ def plot_schedule(scheduler_name, scheduler_cls: torch.optim.lr_scheduler.LRSche
         total_iters_scheduler_kwargs = {"total_steps": STEPS}
     else:
         total_iters_scheduler_kwargs = {}
-    
+
     defaults = DEFAULTS.get(scheduler_name, {}) | parameters
 
     # Convert lambdas strings to functions
     for parameter in defaults:
         if "lambda" in str(defaults[parameter]):
             st.write(f"Converting {defaults[parameter]} to function")
-            defaults[parameter] = eval(defaults[parameter])
+            defaults[parameter] = ast.literal_eval(defaults[parameter])
 
     scheduler_kwargs = defaults | total_iters_scheduler_kwargs
     scheduler = scheduler_cls(optimizer, **scheduler_kwargs)
@@ -65,7 +70,12 @@ def plot_schedule(scheduler_name, scheduler_cls: torch.optim.lr_scheduler.LRSche
     fig.update_yaxes(range=[0, None])
     return fig
 
-def show_config(scheduler_name, scheduler_cls: torch.optim.lr_scheduler.LRScheduler):
+
+def show_config(  # noqa: PLR0912, C901
+    scheduler_name: str,
+    scheduler_cls: torch.optim.lr_scheduler.LRScheduler,
+) -> int | float | bool | Iterable | Callable | str:
+    """Show configuration options for a given scheduler."""
     signature = inspect.signature(scheduler_cls)
     selected_inputs = {}
     for parameter_name in signature.parameters:
@@ -99,35 +109,69 @@ def show_config(scheduler_name, scheduler_cls: torch.optim.lr_scheduler.LRSchedu
         elif annotation is Callable or type(annotation) is _CallableGenericAlias:
             annotation = Callable
             default = parameter.default
-        
+
         if default in [inspect.Signature.empty, None]:
             defaults = DEFAULTS.get(scheduler_name)
             default = defaults.get(str(parameter.name)) if defaults else None
 
         if annotation in (int, float):
-            selected_input = st.slider(f"{parameter_name}", value=default, key=f"{scheduler_name}-{parameter_name}")
+            selected_input = st.slider(
+                f"{parameter_name}",
+                value=default,
+                key=f"{scheduler_name}-{parameter_name}",
+            )
         elif type(annotation) is tuple:
-            selected_input = st.radio(f"{parameter_name}", options=annotation, index=annotation.index(default), key=f"{scheduler_name}-{parameter_name}")
+            selected_input = st.radio(
+                f"{parameter_name}",
+                options=annotation,
+                index=annotation.index(default),
+                key=f"{scheduler_name}-{parameter_name}",
+            )
         elif annotation is bool:
-            selected_input = st.toggle(f"{parameter_name}", value=default, key=f"{scheduler_name}-{parameter_name}")
+            selected_input = st.toggle(
+                f"{parameter_name}",
+                value=default,
+                key=f"{scheduler_name}-{parameter_name}",
+            )
         elif annotation is Iterable:
-            selected_input = st.text_input(f"{parameter_name}", key=f"{scheduler_name}-{parameter_name}", value=default)
-            selected_input = eval(selected_input)
+            selected_input = st.text_input(
+                f"{parameter_name}",
+                key=f"{scheduler_name}-{parameter_name}",
+                value=default,
+            )
+            selected_input = ast.literal_eval(selected_input)
         elif annotation is Callable or type(annotation) is _CallableGenericAlias:
             selected_input = st.text_input(f"{parameter_name}", value=default)
         else:
-            selected_input = st.text_input(f"{parameter_name} (not recognized)", disabled=True, value=default)
+            selected_input = st.text_input(
+                f"{parameter_name} (not recognized)",
+                disabled=True,
+                value=default,
+            )
         selected_inputs[parameter_name] = selected_input
 
     return selected_inputs
 
-def main():
+
+def main() -> None:
+    """Entrypoint for application."""
     st.title("Visualize Learning Rate Schedulers")
 
-    torch_optim_lr_scheduler_members = sorted(inspect.getmembers(torch.optim.lr_scheduler, inspect.isclass), key=lambda x: x[0])
+    torch_optim_lr_scheduler_members = sorted(
+        inspect.getmembers(torch.optim.lr_scheduler, inspect.isclass),
+        key=lambda x: x[0],
+    )
     for member_name, member_cls in torch_optim_lr_scheduler_members:
-        if issubclass(member_cls, torch.optim.lr_scheduler.LRScheduler) and member_name not in ["_LRScheduler", "LRScheduler", "ChainedScheduler", "SequentialLR", "ReduceLROnPlateau"]:
-
+        if issubclass(
+            member_cls,
+            torch.optim.lr_scheduler.LRScheduler,
+        ) and member_name not in [
+            "_LRScheduler",
+            "LRScheduler",
+            "ChainedScheduler",
+            "SequentialLR",
+            "ReduceLROnPlateau",
+        ]:
             plot_col, config_col = st.columns([0.7, 0.3])
 
             with config_col:
@@ -138,12 +182,7 @@ def main():
                 fig.update()
                 st.plotly_chart(fig)
 
-            
             st.divider()
-            
-
-
-
 
 
 if __name__ == "__main__":
